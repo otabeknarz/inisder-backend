1295e04e200c810bb984ee5c51bee4977bc44ea9
Otabek Narz <otabeknarz@gmail.com>
2025-05-22 04:48:31 +0500

    some changes for docker

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,6 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 
 RUN python manage.py collectstatic --noinput
+RUN python manage.py migrate
 
 CMD ["gunicorn", "insider.wsgi:application", "--bind", "0.0.0.0:9742"]
--- insider/settings.py
+++ insider/settings.py
@@ -94,8 +94,10 @@ WSGI_APPLICATION = "insider.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
     }
 }
 
